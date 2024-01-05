ea68a47f823414856777d399c0a560ab838671fc
Daniel R. de Oliveira <daniel.ribeiro.dev@outlook.com>
2024-01-05 19:45:18 -0300

    feat: create input component

---
+++ src/app/components/input/input.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
+import { AutofocusDirective } from '@directives/autofocus.directive';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [ReactiveFormsModule, AutofocusDirective],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.scss',
+})
+export class InputComponent {
+  @Input({ required: true }) _formControl!: FormControl<any>;
+
+  @Input() _id?: string;
+  @Input() _placeholder?: string;
+  @Input() _name?: string;
+  @Input() _type = 'text';
+  @Input() _autoComplete = 'off';
+  @Input() _autoFocus?: boolean;
+}
