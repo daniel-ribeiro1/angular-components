708215938f5e6c47ec02806d50b1755108031d49
Daniel R. de Oliveira <daniel.ribeiro.dev@outlook.com>
2024-01-06 08:56:25 -0300

    refactor: add icons to input component

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
+import { IconComponent } from '@components/icon/icon.component';
 import { AutofocusDirective } from '@directives/autofocus.directive';
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [ReactiveFormsModule, AutofocusDirective],
+  imports: [ReactiveFormsModule, AutofocusDirective, IconComponent],
   templateUrl: './input.component.html',
   styleUrl: './input.component.scss',
 })
@@ -15,8 +16,10 @@ export class InputComponent {
 
   @Input() _id = '';
   @Input() _name = '';
-  @Input() _autoFocus = false
+  @Input() _autoFocus = false;
   @Input() _type = 'text';
   @Input() _autoComplete = 'off';
   @Input() _placeholder = '';
+  @Input() _iconPosition: 'left' | 'right' = 'right';
+  @Input() _icon?: string;
 }
